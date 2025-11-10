8d1ed598d20cf1245ad6337a472e11e8c4ab52d1
brent.zip <2301687@ub.edu.ph>
2025-11-10 18:09:32 +0800

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Attributes from "./react_example_codes/Attributes"
+import CheckBox from "./react_example_codes/checkbox"
+import Components from "./react_example_codes/Components.jsx"
+import Conditionals from "./react_example_codes/conditionals.jsx"
+import Css_modules from "./react_example_codes/css_modules.jsx"
+import Css_styling from "./react_example_codes/css_styling.jsx"
+import Custom_hooks from "./react_example_codes/Custom_hooks.jsx"
+import Destructing_props from "./react_example_codes/destructing_props.jsx"
+import Events from "./react_example_codes/events.jsx"
+import Expression from "./react_example_codes/expression.jsx"
+import Forms from "./react_example_codes/forms.jsx"
+import Forms_Submit from "./react_example_codes/forms_submit.jsx"
+import Forward_Ref from "./react_example_codes/forward_ref.jsx"
+import HOC from "./react_example_codes/HOC.jsx"
+import If_Statements from "./react_example_codes/if_statements.jsx"
+import Inputs from "./react_example_codes/Inputs.jsx"
+import Lists from "./react_example_codes/lists.jsx"
+import Portal from "./react_example_codes/portal.jsx"
+import Props from "./react_example_codes/props.jsx"
+import PropsChildren from "./react_example_codes/propsChildren.jsx"
+import Radio from "./react_example_codes/radio.jsx"
+import React_JSX_intro from "./react_example_codes/React_JSX_intro.jsx"
+import Select from "./react_example_codes/select.jsx"
+import Text_Area from "./react_example_codes/Text_Area.jsx"
+import Transitions from "./react_example_codes/transitions.jsx"
+import Usecallback from "./react_example_codes/useCallback.jsx"
+import Useeffect from "./react_example_codes/useEffect.jsx"
+import Usereducer from "./react_example_codes/useReducer.jsx"
+import Useref from "./react_example_codes/useRef.jsx"
+import Usestate from "./react_example_codes/usestate.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+function App(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2 style={{ textAlign: "center", marginTop: "40px" }}>Attributes</h2>
+    <Attributes />
+
+    <h2 style={{ textAlign: "center", marginTop: "40px" }}>Checkbox</h2>
+    <CheckBox />
+    
+    <h2 style={{ textAlign: "center", marginTop: "40px" }}>components</h2>
+    <Components />
+
+    <h2 style={{ textAlign: "center", marginTop: "40px" }}>components</h2>
+    <Conditionals />
+
+    <h2 style={{ textAlign: "center", marginTop: "40px" }}>components</h2>
+    <Css_modules />
+
+
+    <Css_styling />
+    
+
+    <Custom_hooks />
+
+
+    <Destructing_props />   
+    
+
+    <Events />
+
+
+    <Expression />
+
+
+    <Forms_Submit />
+
+
+    <Forms />
+
+    
+    <Forward_Ref />
+
+    <HOC />
+
+    <If_Statements />
+
+    <Inputs />
+
+    <Lists />
+
+    <Portal />
+
+    <Props />
+
+    <PropsChildren />
+
+    <Radio />
+
+    <React_JSX_intro />
+
+    <Select />
+
+    <Transitions />
+
+    <Text_Area />
+
+    <Usecallback />
+
+    <Useeffect />
+
+    <Usereducer />
+
+    <Useref />
+
+    <Usestate />
+
     </>
+    
+    
   )
 }
 
 export default App
+
+
